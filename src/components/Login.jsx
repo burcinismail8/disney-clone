693764b437426aa8fe4ada77d4016a398dc281a3
Burchin Ismail <burcinismail8@gmail.com>
2022-05-06 12:23:55 +0300

    Added signup button

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,6 +7,7 @@ const Login = (props) => {
       <Content>
         <CTA>
           <CTALogoOne src="/images/cta-logo-one.svg" alt="" />
+          <SignUp>GET ALL THERE</SignUp>
         </CTA>
         <BgImage />
       </Content>
@@ -62,5 +63,22 @@ const CTALogoOne = styled.img`
   min-height: 1px;
   display: block;
   width: 100%;
+`;
+
+const SignUp = styled.a`
+  font-weight: bold;
+  color: #f9f9f9;
+  background-color: #0063e5;
+  margin-bottom: 12px;
+  width: 100%;
+  letter-spacing: 1.5px;
+  font-size: 18px;
+  padding: 20px;
+  border: 1px solid transparent;
+  border-radius: 4px;
+
+  &:hover {
+    background-color: #0483ee;
+  }
 `;
 export default Login;
