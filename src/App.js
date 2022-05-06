75fe65ca5c9665d07df4df184b23ae7aae9df0ed
Burchin Ismail <burcinismail8@gmail.com>
2022-05-06 10:43:59 +0300

    Global styling

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h2>HEllo</h2>
+      <h2>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget
+        viverra velit, in volutpat ante. Etiam ultrices ante quis lobortis
+        vulputate. Quisque maximus consequat eros nec volutpat.{" "}
+      </h2>
     </div>
   );
 }
