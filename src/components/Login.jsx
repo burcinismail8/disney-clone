85969277c81899b690525ef38381f25e31242e4b
Burchin Ismail <burcinismail8@gmail.com>
2022-05-07 10:03:15 +0300

    Added navbar and navmenu

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -13,6 +13,7 @@ const Login = (props) => {
             with a Disney+ subscription. As of 03/26/21, the price of Disney+
             and The Disney Bundle will increase by $1.
           </Description>
+          <CTALogoTwo src="/images/cta-logo-two.png" alt="" />
         </CTA>
         <BgImage />
       </Content>
@@ -81,6 +82,7 @@ const SignUp = styled.a`
   padding: 20px;
   border: 1px solid transparent;
   border-radius: 4px;
+  cursor: pointer;
 
   &:hover {
     background-color: #0483ee;
@@ -93,6 +95,13 @@ const Description = styled.p`
   margin: 0 0 24px;
   line-height: 1.5;
   letter-spacing: 1.5px;
+`;
+
+const CTALogoTwo = styled.img`
+  margin-bottom: 20px;
+  display: inline-block;
+  vertical-align: bottom;
+  width: 100%;
 `;
 
 export default Login;
